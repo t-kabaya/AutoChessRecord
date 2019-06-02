7fffe6cc528aae74a3ff65e46e71cab58044a934
soniczsonic <kabaya.dori@gmail.com>
2019-06-02 14:43:34 +0900

    style: added styles to selectUnitScreen

--- screens/SelectUnitsScreen.js
+++ screens/SelectUnitsScreen.js
@@ -7,26 +7,53 @@ import {
   Text,
   TouchableOpacity,
   View,
-  FlatList
+  FlatList,
 } from 'react-native'
+import Layout from '../constants/Layout'
 import unitData, { unitImagePathArray } from '../constants/UnitData'
+import {Badge} from 'native-base'
+
+// unitの重複は考えない
 
 export default class SelectUnitsScreen extends React.Component {
+  state = {
+    unitState: unitData.map(data => ({...data, level: 0}))
+  }
+
   unitListItem = ({ item }) => {
     return (
-      <View>
-        <Image source={unitImagePathArray[item.id - 1]} />
+      <View style={styles.unitListItemContainer}>
+        <Badge><Text>Lv{item.level}</Text></Badge>
+        <Image resizeMode='contain' style={styles.unitListItemImage} source={unitImagePathArray[item.id - 1]} />
         <Text>{item.unitName}</Text>
       </View>
     )
   }
 
   render () {
+    const {unitState} = this.state
     return (
       <View>
         <Text>選択してください</Text>
-        <FlatList data={unitData} renderItem={this.unitListItem} />
+        <FlatList numColumns={4} data={unitState} renderItem={this.unitListItem} />
+
+        {/* <FlatList numColumns={4} data={unitData} renderItem={this.unitListItem} /> */}
       </View>
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  unitListItemContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: Layout.window.width / 4,
+  },
+  unitListItemImage: {
+    width: Layout.window.width / 5,
+    height: Layout.window.width / 5,
+    borderWidth: 5,
+    borderColor: '#ffe084'
+  }
+})
