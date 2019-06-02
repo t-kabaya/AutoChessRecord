7c77ab92e7e1ec96652d95a9af54462b09c6d99a
soniczsonic <kabaya.dori@gmail.com>
2019-06-02 16:44:49 +0900

    feature: added select unit screen

--- constants/Colors.js
+++ constants/Colors.js
@@ -4,11 +4,16 @@ export default {
   tintColor,
   tabIconDefault: '#ccc',
   tabIconSelected: tintColor,
-  tabBar: '#fefefe',
+  tabBar: '#fefefe',  
   errorBackground: 'red',
   errorText: '#fff',
   warningBackground: '#EAEB5E',
   warningText: '#666804',
   noticeBackground: tintColor,
   noticeText: '#fff',
+
+  level0Green: '#6bf442',
+  level1Blue: '#4286fa',
+  level2Yellow: '#ebf442',
+  level3Red: '#f44542'
 };
--- screens/SelectUnitsScreen.js
+++ screens/SelectUnitsScreen.js
@@ -8,10 +8,13 @@ import {
   TouchableOpacity,
   View,
   FlatList,
+  TouchableHighlight,
+  TouchableWithoutFeedback
 } from 'react-native'
 import Layout from '../constants/Layout'
+import Colors from '../constants/Colors'
 import unitData, { unitImagePathArray } from '../constants/UnitData'
-import {Badge} from 'native-base'
+import {Badge, Button} from 'native-base'
 
 // unitの重複は考えない
 
@@ -20,24 +23,49 @@ export default class SelectUnitsScreen extends React.Component {
     unitState: unitData.map(data => ({...data, level: 0}))
   }
 
+  onPressUnitListItem = (item) => {
+    const unitId = item.id
+    const unitState = this.state.unitState.map(unit => {
+      if (unit.id === unitId) {
+        return ({...unit, level: (unit.level + 1) % 4})
+      } else {
+        return unit
+      }
+    })
+    this.setState({unitState})
+  }
+
   unitListItem = ({ item }) => {
+    let levelColor
     return (
-      <View style={styles.unitListItemContainer}>
-        <Badge><Text>Lv{item.level}</Text></Badge>
-        <Image resizeMode='contain' style={styles.unitListItemImage} source={unitImagePathArray[item.id - 1]} />
-        <Text>{item.unitName}</Text>
-      </View>
+      <TouchableWithoutFeedback onPress={() => this.onPressUnitListItem(item)}>
+        <View style={styles.unitListItemContainer}>
+          <View>
+            <Text>Lv{item.level}</Text>
+          </View>
+          <Image resizeMode='contain' style={styles.unitListItemImage} source={unitImagePathArray[item.id - 1]} />
+          <Text>{item.unitName}</Text>
+        </View>
+      </TouchableWithoutFeedback>
     )
   }
 
   render () {
     const {unitState} = this.state
+    const selectedUnits = unitState.filter(unit => !(unit.level === 0))
+    console.log(selectedUnits)
     return (
-      <View>
-        <Text>選択してください</Text>
-        <FlatList numColumns={4} data={unitState} renderItem={this.unitListItem} />
-
-        {/* <FlatList numColumns={4} data={unitData} renderItem={this.unitListItem} /> */}
+      <View style={{flex: 1}}>
+        <View style={{flex: 4}}>
+          <Button full>
+            <Text style={styles.doneSelectButtonText}>決定</Text>
+            </Button>
+          <FlatList style={styles.unitListContainer} numColumns={4} data={unitState} renderItem={this.unitListItem} />
+        </View>
+        <View style={{flex: 1}}>
+          <Text style={styles.text}>選択済み</Text>
+          <FlatList inverted horizontal data={selectedUnits} renderItem={this.unitListItem} />
+        </View>
       </View>
     )
   }
@@ -48,12 +76,24 @@ const styles = StyleSheet.create({
   unitListItemContainer: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: Layout.window.width / 4,
+    width: Layout.width / 4,
+  },
+  unitListContainer: {
+    height: Layout.height * 4 / 5
   },
   unitListItemImage: {
-    width: Layout.window.width / 5,
-    height: Layout.window.width / 5,
+    width: Layout.width / 5,
+    height: Layout.width / 5,
     borderWidth: 5,
     borderColor: '#ffe084'
+  },
+  doneSelectButtonText: {
+    color: 'white',
+    fontSize: 20
+  },
+  text: {
+    alignSelf: 'center',
+    paddingTop: 5,
+    fontSize: 18,
   }
 })
