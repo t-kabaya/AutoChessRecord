8f21a371743c78b9010cf78d5a82d9605ff86970
soniczsonic <kabaya.dori@gmail.com>
2019-06-16 13:28:11 +0900

    feature: hide statusbar

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle='default' />}
+          <StatusBar hidden />
           <AppNavigator />
         </View>
       )
