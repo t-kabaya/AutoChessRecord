c5264482b42f1d26b0f40d433c65498fedc24a12
soniczsonic <kabaya.dori@gmail.com>
2019-06-23 14:06:37 +0900

    feature: add new query to get highest win rate units

--- fireStore/MatchRecordORM.js
+++ fireStore/MatchRecordORM.js
@@ -1,7 +1,7 @@
-import { db } from './fireStoreSetup'
-import { Constants } from 'expo'
-import unitData from '../constants/UnitData'
-import calcSynergiesFromUnitIds from '../utils/calcSynergiesFromUnitIds'
+import { db } from "./fireStoreSetup";
+import { Constants } from "expo";
+import unitData from "../constants/UnitData";
+import calcSynergiesFromUnitIds from "../utils/calcSynergiesFromUnitIds";
 
 // 設計
 // record table
@@ -18,43 +18,65 @@ import calcSynergiesFromUnitIds from '../utils/calcSynergiesFromUnitIds'
 export const saveMatchRecordToFireStore = async (units, ranking) => {
   const unitDataToStore = units
     .filter(unit => !(unit.level === 0))
-    .map(unit => ({ unitId: unit.unitId, level: unit.level }))
+    .map(unit => ({ unitId: unit.unitId, level: unit.level }));
   try {
-    db.collection('matchRecord').add({
+    db.collection("matchRecord").add({
       userId: Constants.installationId,
       ranking: 3,
       units: unitDataToStore,
-      date: JSON.stringify(new Date())
-    })
+      date: JSON.stringify(new Date()),
+    });
   } catch (error) {
-    throw new Error('error at saveMatchRecordToFireStore')
+    throw new Error("error at saveMatchRecordToFireStore");
   }
-}
+};
 
 export const getMyMatchRecord = async () => {
   // 開発のため、モックデータを使用する。
-  return mockDataOfOpDeck
+  return mockDataOfOpDeck;
 
   try {
-    const response = await db.collection('matchRecord').get()
+    const response = await db.collection("matchRecord").get();
     const myMatchRecord = await response.docs
       .map(item => item.data())
-      .filter(data => data.userId === Constants.installationId)
+      .filter(data => data.userId === Constants.installationId);
 
     // const myMatchRecordHasImage = myMatchRecord.map(record => ({...record, }))
     // ここに、シナジーなどを計算する処理を書いていくが、また後日追加する。
     const myMatchRecordWithSynergyLevel = myMatchRecord.map(record => {
-      const synergy = calcSynergiesFromUnitIds(record.units)
-      debugger
+      const synergy = calcSynergiesFromUnitIds(record.units);
 
-      return { ...record, synergy }
-    })
-    debugger
-    return myMatchRecordWithSynergyLevel
+      return { ...record, synergy };
+    });
+    return myMatchRecordWithSynergyLevel;
   } catch (error) {
-    console.log('error')
+    console.log("error");
   }
-}
+};
+
+// top3率が高いユニットを示す。
+export const getWinRateOfUnit = async () => {
+  const response = await db.collection("matchRecord").get();
+  // const myMatchRecord = await response.docs.map(item => item.data());
+  //   var citiesRef = db.collection('cities');
+  // var allCities = citiesRef.get()
+  //     .then(snapshot => {
+  //       snapshot.forEach(doc => {
+  //         console.log(doc.id, '=>', doc.data());
+  //       });
+  //     })
+  //     .catch(err => {
+  //       console.log('Error getting documents', err);
+  //     });
+
+  // const snapShot = await db.collection('users').get();
+  // docsをmapする
+  const data = response.docs.map(doc => {
+    return doc.data();
+  });
+
+  return data;
+};
 
 const mockDataOfOpDeck = [
   {
@@ -63,44 +85,44 @@ const mockDataOfOpDeck = [
     units: [
       {
         level: 1,
-        unitId: 2
+        unitId: 2,
       },
       {
         level: 1,
-        unitId: 3
+        unitId: 3,
       },
       {
         level: 1,
-        unitId: 4
+        unitId: 4,
       },
       {
         level: 1,
-        unitId: 5
+        unitId: 5,
       },
       {
         level: 2,
-        unitId: 6
+        unitId: 6,
       },
       {
         level: 1,
-        unitId: 7
-      }
+        unitId: 7,
+      },
     ],
-    userId: '1802A72F-12B4-4EA7-86E1-D6BDFA60F5A2',
+    userId: "1802A72F-12B4-4EA7-86E1-D6BDFA60F5A2",
     synergy: [
       {
         synergyLevel: 2,
-        synergy: 'marine'
+        synergy: "marine",
       },
       {
         synergyLevel: 2,
-        synergy: 'psyker'
+        synergy: "psyker",
       },
       {
         synergyLevel: 2,
-        synergy: 'supporter'
-      }
-    ]
+        synergy: "supporter",
+      },
+    ],
   },
   {
     date: '"2019-06-16T07:33:36.358Z"',
@@ -108,22 +130,22 @@ const mockDataOfOpDeck = [
     units: [
       {
         level: 1,
-        unitId: 2
+        unitId: 2,
       },
       {
         level: 1,
-        unitId: 3
+        unitId: 3,
       },
       {
         level: 1,
-        unitId: 4
+        unitId: 4,
       },
       {
         level: 1,
-        unitId: 6
-      }
+        unitId: 6,
+      },
     ],
-    userId: '1802A72F-12B4-4EA7-86E1-D6BDFA60F5A2',
-    synergy: []
-  }
-]
+    userId: "1802A72F-12B4-4EA7-86E1-D6BDFA60F5A2",
+    synergy: [],
+  },
+];
