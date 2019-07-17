72c7078e3351a4e2707079fadda23c94db2a126e
IPG <ipg@IPGnoMacBook-puro.local>
2019-07-17 21:02:55 +0900

    chore: add secret key in Constants.manifest

--- fireStore/fireStoreSetup.js
+++ fireStore/fireStoreSetup.js
@@ -1,17 +1,8 @@
 import firebase from 'firebase'
 import '@firebase/firestore'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBd0ecWHnGXvLdbtyV3LSQvACUdEvoI39Q",
-  authDomain: "autochessrecord.firebaseapp.com",
-  databaseURL: "https://autochessrecord.firebaseio.com",
-  projectId: "autochessrecord",
-  storageBucket: "autochessrecord.appspot.com",
-  messagingSenderId: "990548586835",
-  appId: "1:990548586835:web:5a00c7cd7b232d99"
-};
+import { Constants } from 'expo'
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(Constants.manifest.extra.firebaseConfig)
 
-export const db = firebase.firestore();
+export const db = firebase.firestore()
