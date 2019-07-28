38baa5f69f7db0aaa27d12e1d90d9116fb9bc09c
soniczsonic <kabaya.dori@gmail.com>
2019-07-28 18:02:19 +0900

    feature: added layoutAnimation. however, I will rewite this code to use Animated

--- components/TabBar.js
+++ components/TabBar.js
@@ -1,51 +1,110 @@
-import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  NativeModules,
+  LayoutAnimation
+} from 'react-native'
+
+const { UIManager } = NativeModules
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true)
 
 const S = StyleSheet.create({
   container: { flexDirection: 'row', height: 52, elevation: 2 },
   tabButton: { flex: 1, justifyContent: 'center', alignItems: 'center' }
 })
 
-const TabBar = props => {
-  const {
-    renderIcon,
-    getLabelText,
-    activeTintColor,
-    inactiveTintColor,
-    onTabPress,
-    onTabLongPress,
-    getAccessibilityLabel,
-    navigation
-  } = props
+class TabBar extends Component {
+  render () {
+    const { routes } = this.props.navigation.state
+    return (
+      <View style={S.container}>
+        {routes.map((route, routeIndex) => {
+          return (
+            <TabBarItem
+              tabProps={this.props}
+              route={route}
+              routeIndex={routeIndex}
+            />
+          )
+        })}
+      </View>
+    )
+  }
+}
 
-  const { routes, index: activeRouteIndex } = navigation.state
+class TabBarItem extends Component {
+  state = {
+    animationStyle: {}
+  }
 
-  return (
-    <View style={S.container}>
-      {routes.map((route, routeIndex) => {
-        const isRouteActive = routeIndex === activeRouteIndex
-        const tintColor = isRouteActive ? activeTintColor : inactiveTintColor
+  componentDidMount = () => {
+    // 初期アニメーションをセット
+    LayoutAnimation.spring()
+    this.setState({ animationStyle: this.inactivieAnimationStyle })
+  }
 
-        return (
-          <TouchableOpacity
-            key={routeIndex}
-            style={S.tabButton}
-            onPress={() => {
-              onTabPress({ route })
-            }}
-            onLongPress={() => {
-              onTabLongPress({ route })
-            }}
-            accessibilityLabel={getAccessibilityLabel({ route })}
-          >
-            {renderIcon({ route, focused: isRouteActive, tintColor })}
+  inactivieAnimationStyle = {
+    height: 50,
+    backgroundColor: 'green'
+  }
 
-            <Text>{getLabelText({ route })}</Text>
-          </TouchableOpacity>
-        )
-      })}
-    </View>
-  )
+  activeAnimationStyle = {
+    height: 20,
+    backgroundColor: 'blue'
+  }
+
+  componentDidUpdate (prevProps) {
+    const isTabSelectionChanged =
+      prevProps.tabProps.navigation.state.index !==
+      this.props.tabProps.navigation.state.index
+    if (isTabSelectionChanged) {
+      // alert(this.props.props.navigation.state.index)
+      isTabItemSelected =
+        this.props.tabProps.navigation.state.index == this.props.routeIndex
+      if (isTabItemSelected) {
+        // LayoutAnimation.spring()
+        this.setState({ animationStyle: this.activeAnimationStyle })
+      } else {
+        this.setState({ animationStyle: this.inactivieAnimationStyle })
+      }
+    }
+  }
+
+  render () {
+    const {
+      navigation,
+      isRouteActive,
+      getAccessibilityLabel,
+      getLabelText,
+      onTabPress,
+      onTabLongPress
+    } = this.props.tabProps
+
+    const { route, routeIndex } = this.props
+
+    const isActiveRoute = navigation.state.index === routeIndex
+    return (
+      <TouchableOpacity
+        activeOpacity={0.8}
+        key={routeIndex}
+        style={[
+          S.tabButton,
+          { backgroundColor: isActiveRoute ? 'red' : 'white' }
+        ]}
+        onPress={() => onTabPress({ route })}
+        onLongPress={() => onTabLongPress({ route })}
+        accessibilityLabel={getAccessibilityLabel({ route })}
+      >
+        <View style={this.state.animationStyle}>
+          <Text>{getLabelText({ route })}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
 }
 
 export default TabBar
