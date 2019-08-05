398ea58300303cadf12ec27fa88d58d2807b9803
soniczsonic <kabaya.dori@gmail.com>
2019-08-05 21:31:31 +0900

    businessLogic: add isValidUnits

--- screens/SelectUnits/BusinessLogic.ts
+++ screens/SelectUnits/BusinessLogic.ts
@@ -6,3 +6,10 @@ export const removeAbnormalUnit = (units) => {
 export const isValidRank = (ranking: number): boolean => (
   typeof ranking === 'number' && ranking >= 1 && ranking <= 8
 )
+
+// units = [{unitId: 7, level: 3}]のような感じ。
+// 星が３以上と、1以下のユニット、untisIdがないユニットは弾く
+export const isValidUnits = (units: object[]): boolean => {
+  if (units == null) return
+  return false
+}
