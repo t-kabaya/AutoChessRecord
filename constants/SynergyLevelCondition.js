5b849ae4f4138a0fbf449563353352d8889a29bf
IPG <ipg@IPGnoMacBook-puro.local>
2019-08-11 22:50:37 +0900

    bug: fix not detect mech4

--- constants/SynergyLevelCondition.js
+++ constants/SynergyLevelCondition.js
@@ -20,38 +20,53 @@ export const synergyLevel3Condition = [
 export const synergyLevel2Condition = [
   // サモナーとライダーとウォーカーとパンダはlevel1のみ。
   // シージェとロングショットは６(全６体)でlevel2
+  /* -------------------- 6 units --------------------- */
   { synergy: en.siegeBreaker, count: 6 },
+  { synergy: en.siegeBreaker, count: 7 },
+
   { synergy: en.longShot, count: 6 },
+  { synergy: en.longShot, count: 7 },
+
+  { synergy: en.blaster, count: 6 },
+  { synergy: en.blaster, count: 7 },
+
+  { synergy: en.assassin, count: 6 },
+  { synergy: en.assassin, count: 7 },
 
   /* -------------------- 4 units --------------------- */
   /* -------------------- job --------------------- */
+  { synergy: en.guardian, count: 4 },
   { synergy: en.guardian, count: 5 },
-  { synergy: en.blaster, count: 5 },
-  { synergy: en.supporter, count: 5 },
-  { synergy: en.assassin, count: 5 },
-  { synergy: en.vanguard, count: 5 },
-  /* -------------------- race --------------------- */
-  { synergy: en.marine, count: 5 },
-  { synergy: en.mech, count: 5 },
-  { synergy: en.airForce, count: 5 },
-  { synergy: en.puppet, count: 5 },
-  { synergy: en.psyker, count: 5 },
-  { synergy: en.kraken, count: 5 },
-  { synergy: en.beast, count: 5 },
-  { synergy: en.rapter, count: 5 },
 
-  /* -------------------- 5 units --------------------- */
-  /* -------------------- job --------------------- */
-  { synergy: en.guardian, count: 5 },
-  { synergy: en.blaster, count: 5 },
-  { synergy: en.assassin, count: 5 },
+  { synergy: en.supporter, count: 4 },
+  { synergy: en.supporter, count: 5 },
+
+  { synergy: en.vanguard, count: 3 },
+  { synergy: en.vanguard, count: 4 },
   { synergy: en.vanguard, count: 5 },
-  /* -------------------- race --------------------- */
+
+  { synergy: en.marine, count: 4 },
   { synergy: en.marine, count: 5 },
+
+  { synergy: en.mech, count: 4 },
   { synergy: en.mech, count: 5 },
+
+  { synergy: en.airForce, count: 4 },
+  { synergy: en.airForce, count: 5 },
+
+  { synergy: en.puppet, count: 4 },
   { synergy: en.puppet, count: 5 },
+
+  { synergy: en.psyker, count: 4 },
+  { synergy: en.psyker, count: 5 },
+
+  { synergy: en.kraken, count: 4 },
   { synergy: en.kraken, count: 5 },
+
+  { synergy: en.beast, count: 4 },
   { synergy: en.beast, count: 5 },
+
+  { synergy: en.rapter, count: 4 },
   { synergy: en.rapter, count: 5 }
 ]
 
@@ -60,8 +75,9 @@ export const synergyLevel1Condition = [
   { synergy: en.walker, count: 1 },
 
   // サモナーとパンダは３（全３体）でlevel1
-  { synergy: en.summoner, count: 3 },
+  { synergy: en.summoner, count: 2 },
   { synergy: en.panda, count: 3 },
+  { synergy: en.rider, count: 2 },
   // シージェとロングショットは3でlevel1
   { synergy: en.siegeBreaker, count: 3 },
   { synergy: en.siegeBreaker, count: 4 },
@@ -70,38 +86,48 @@ export const synergyLevel1Condition = [
   { synergy: en.longShot, count: 4 },
   { synergy: en.longShot, count: 5 },
 
-  /* -------------------- count 2 --------------------- */
   { synergy: en.guardian, count: 2 },
+  { synergy: en.guardian, count: 3 },
+
   { synergy: en.supporter, count: 2 },
-  { synergy: en.assassin, count: 2 },
-  { synergy: en.vanguard, count: 2 },
+  { synergy: en.supporter, count: 3 },
+
+  { synergy: en.assassin, count: 3 },
+  { synergy: en.assassin, count: 4 },
+  { synergy: en.assassin, count: 5 },
+
+  { synergy: en.vanguard, count: 3 },
+  { synergy: en.vanguard, count: 4 },
+  { synergy: en.vanguard, count: 5 },
+
   { synergy: en.marine, count: 2 },
-  { synergy: en.rider, count: 2 },
+  { synergy: en.marine, count: 3 },
+
   { synergy: en.mech, count: 2 },
+  { synergy: en.mech, count: 3 },
+
   { synergy: en.airForce, count: 2 },
+  { synergy: en.airForce, count: 3 },
+
   { synergy: en.puppet, count: 2 },
+  { synergy: en.puppet, count: 3 },
+
   { synergy: en.psyker, count: 2 },
+  { synergy: en.psyker, count: 3 },
+
   { synergy: en.kraken, count: 2 },
+  { synergy: en.kraken, count: 3 },
+
   { synergy: en.beast, count: 2 },
+  { synergy: en.beast, count: 3 },
+
   { synergy: en.rapter, count: 2 },
+  { synergy: en.rapter, count: 3 },
+
   { synergy: en.insectoid, count: 2 },
+  { synergy: en.insectoid, count: 3 },
+
   { synergy: en.immortal, count: 2 },
 
-  /* -------------------- count3 --------------------- */
-  { synergy: en.guardian, count: 3 },
-  { synergy: en.blaster, count: 3 },
-  { synergy: en.supporter, count: 3 },
-  { synergy: en.assassin, count: 3 },
-  { synergy: en.vanguard, count: 3 },
-  { synergy: en.marine, count: 3 },
-  { synergy: en.rider, count: 3 },
-  { synergy: en.mech, count: 3 },
-  { synergy: en.airForce, count: 3 },
-  { synergy: en.puppet, count: 3 },
-  { synergy: en.psyker, count: 3 },
-  { synergy: en.kraken, count: 3 },
-  { synergy: en.beast, count: 3 },
-  { synergy: en.rapter, count: 3 },
-  { synergy: en.insectoid, count: 3 },
-  { synergy: en.immortal, count: 3 }
+  { synergy: en.blaster, count: 3 }
 ]
